9fca01a67c8681055f7a0baa7008eda9d7cf99a0
Christopher-Pierson <54875547+Christopher-Pierson@users.noreply.github.com>
2020-03-04 12:59:10 -0600

    ProportionalSymbolsWorking

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 /* Javascript by Christopher Pierson, 2020 */
 
 /* Map of GeoJSON data from MegaCities.geojson */
-//declare map var in global scope
+//declare vars globally so all functions have access
 var mymap;
+var minValue;
+
 
 //function to instantiate the Leaflet map
 function createMap(){
@@ -39,25 +41,87 @@ function onEachFeature(feature, layer) {
     };
 };
 
+function calcMinValue(data){
+
+     //create empty array to store all data values
+     var allValues = [];
+
+     //loop through each city
+     for(var team of data.features){
+          //loop through each year
+          for(var year = 2006; year <= 2019; year+=1){
+                //get population for current year
+               var value = team.properties["Total_"+ String(year)];
+               //add value to array
+              if (value > 0){
+                allValues.push(value)
+
+              }
+           }
+     }
+
+     //get minimum value of our array
+     var minValue = Math.min(...allValues)
+
+     return minValue;
+}
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+
+     //constant factor adjusts symbol sizes evenly
+     var minRadius = 3;
+
+     //Flannery Appearance Compensation formula
+     var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    console.log(radius)
+    return radius;
+};
+
+
+function createPropSymbols(data){
+    //determine the attribute for scaling the proportional symbols
+    var attribute = "Total_2019";
+    //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff1694",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            //for each feature, determine its value for the selected attribute
+            var attValue = Number(feature.properties[attribute]);
+
+            //give each feature's circle marker a radius based on its attribute value
+            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+            //examine the attribute value to check that it is correct
+            //console.log(feature.properties, attValue);
+
+            //create circle markers
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(mymap);
+};
+
 //function to retrieve the data and place it on the map
 function getData(map){
     //load the data
-    $.getJSON("data/nhl_playoffteams_1819_regularseason.geojson", function(response){
-      var geojsonMarkerOptions = {
-      							 radius: 8,
-      							 fillColor: "#ff1694",
-      							 color: "#000",
-      							 weight: 1,
-      							 opacity: 1,
-      							 fillOpacity: 0.8
-      					 };
+    $.getJSON("data/NHL_PlayoffWins_06-19_alt.geojson", function(response){
 
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-                pointToLayer: function (feature, latlng){
-                  return L.circleMarker(latlng, geojsonMarkerOptions)},
-                onEachFeature: onEachFeature
-            }).addTo(mymap);
+      //calculate minimum data value
+      minValue = calcMinValue(response);
+
+      //call function to create proportional symbols
+      createPropSymbols(response);
+
     });
 };
 
